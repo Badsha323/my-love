a1f9d1bbd69f38d8d100d30476fb1d8e064720d7
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-06-04 13:50:46 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -22,7 +22,7 @@ async def index_files(bot, query):
     if raju == 'reject':
         await query.message.delete()
         await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been decliened by our moderators.',
+                               f'Sorry I Only Index Files Document Video Types Files. if Your {chat} Have Posts With Links You Can use This Bot To Add In Inline @InlineSearch_MovieBot.',
                                reply_to_message_id=int(lst_msg_id))
         return
 
@@ -116,7 +116,7 @@ async def send_for_index(bot, message):
     await bot.send_message(LOG_CHANNEL,
                            f'#IndexRequest\n\nBy : {message.from_user.mention} (<code>{message.from_user.id}</code>)\nChat ID/ Username - <code> {chat_id}</code>\nLast Message ID - <code>{last_msg_id}</code>\nInviteLink - {link}',
                            reply_markup=reply_markup)
-    await message.reply('ThankYou For the Contribution, Wait For My Moderators to verify the files.')
+    await message.reply('ThankYou For the Contribution, Wait For My Ai to verify the files.')
 
 
 @Client.on_message(filters.command('setskip') & filters.user(ADMINS))
