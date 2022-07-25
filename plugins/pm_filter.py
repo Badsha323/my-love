3132b974473acd5f0a30b37459efeb6d7754332c
Badsha323 <98164078+Badsha323@users.noreply.github.com>
2022-07-25 14:31:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -169,8 +169,7 @@ async def next_page(bot, query):
 
     if not files:
         return
-     search_msg = await message.reply_sticker(sticker='CAACAgIAAxkBAAEE-d1ipaeEBQABkYqzvvZYJL56zS218NcAAuUAA1advQoICxZklQXRiiQE')
-    settings = await get_settings(query.message.chat.id)
+         settings = await get_settings(query.message.chat.id)
     if settings['button']:
         btn = [
             [
